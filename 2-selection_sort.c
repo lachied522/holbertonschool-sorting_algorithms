310599b10a25762aafafa7cc0e76c32270daaeed
root <lachie.duncan12@gmail.com>
2025-01-11 23:37:29 +0000

    Only swap items if necessary

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -28,9 +28,13 @@ void selection_sort(int *array, size_t size)
 			}
 		}
 
-		temp = array[i];
-		array[i] = array[min];
-		array[min] = temp;
-		print_array(array, size);
+		/* only swap if necessary */
+		if (min != i)
+		{
+			temp = array[i];
+			array[i] = array[min];
+			array[min] = temp;
+			print_array(array, size);
+		}
 	}
 }
