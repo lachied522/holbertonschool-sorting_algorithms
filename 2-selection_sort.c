7d4c23de4c295e5db7e1c278c0cdb09452383341
root <lachie.duncan12@gmail.com>
2025-01-11 23:32:32 +0000

    Selection sort

---
+++ 2-selection_sort.c
@@ -0,0 +1,36 @@
+#include "sort.h"
+
+/**
+ * selection - sorts an array of integers with insertion
+ * @array: integer array
+ * @size: size of array
+ * Return: nothing
+ */
+void selection_sort(int *array, size_t size)
+{
+	size_t i, j, min;
+	int temp;
+
+	if (array == NULL)
+		return;
+
+	for (i = 0; i < size; i++)
+	{
+		/* set min index to current index */
+		min = i;
+
+		for (j = i + 1; j < size; j++)
+		{
+			if (array[j] < array[min])
+			{
+				/* update min index */
+				min = j;
+			}
+		}
+
+		temp = array[i];
+		array[i] = array[min];
+		array[min] = temp;
+		print_array(array, size);
+	}
+}
